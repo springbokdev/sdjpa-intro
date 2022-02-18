27530dd8488f25d2fe07a70766b9b3303dac311a
johnspangenberg <dev@springbok.space>
2022-02-18 16:49:54 +0100

    Renmoved unused import

--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,7 +1,6 @@
 package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.repositories.BookRepository;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
